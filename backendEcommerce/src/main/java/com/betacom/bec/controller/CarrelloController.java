fdb1eb28b127244ed4927bb7d7667a490dbb0aa5
AndreaCarpe4 <andreacarpentieri1998@outlook.it>
2025-02-10 12:01:22 +0100

    prova questi su postman

--- backendEcommerce/src/main/java/com/betacom/bec/controller/CarrelloController.java
+++ backendEcommerce/src/main/java/com/betacom/bec/controller/CarrelloController.java
@@ -3,7 +3,6 @@ package com.betacom.bec.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,23 +55,6 @@ public class CarrelloController {
 	    return response;
 	}
 	
-	@PostMapping("/aggiungi")
-	public ResponseObject<Carrello> aggiungiProdotto(@RequestParam int utenteId, 
-	                                                 @RequestParam int prodottoId, 
-	                                                 @RequestParam int quantita) {
-	    log.debug("aggiungiProdotto: utenteId=" + utenteId + ", prodottoId=" + prodottoId + ", quantita=" + quantita);
-	    ResponseObject<Carrello> response = new ResponseObject<>();
-	    response.setRc(true);
-	    try {
-	        Carrello carrello = carrelloS.aggiungiProdotto(utenteId, prodottoId, quantita);
-	        response.setDati(carrello);
-	    } catch (Exception e) {
-	        response.setMsg(e.getMessage());
-	        response.setRc(false);
-	    }
-	    return response;
-	}
-	
 	
 	@PostMapping("/rimuovi")
 	public ResponseBase rimuoviProdotto(@RequestParam int carrelloId) {
@@ -87,6 +69,50 @@ public class CarrelloController {
 	    }
 	    return response;
 	}
+	
+	@PostMapping("/aggiungi")
+    public ResponseObject<Carrello> aggiungiProdotto(@RequestParam int utenteId, @RequestParam int prodottoId, @RequestParam int quantita) {
+        log.debug("aggiungiProdotto: utenteId=" + utenteId + ", prodottoId=" + prodottoId + ", quantita=" + quantita);
+        ResponseObject<Carrello> response = new ResponseObject<>();
+        response.setRc(true);
+        try {
+            Carrello carrello = carrelloS.aggiungiProdotto(utenteId, prodottoId, quantita);
+            response.setDati(carrello);
+        } catch (Exception e) {
+            response.setMsg(e.getMessage());
+            response.setRc(false);
+        }
+        return response;
+    }
+
+    @PostMapping("/aggiornaPrezzi")
+    public ResponseBase aggiornaPrezziCarrello(@RequestParam int utenteId) {
+        log.debug("aggiornaPrezziCarrello: utenteId=" + utenteId);
+        ResponseBase response = new ResponseBase();
+        response.setRc(true);
+        try {
+            carrelloS.aggiornaPrezziCarrello(utenteId);
+        } catch (Exception e) {
+            response.setMsg(e.getMessage());
+            response.setRc(false);
+        }
+        return response;
+    }
+
+    @GetMapping("/totale")
+    public ResponseObject<Double> calcolaTotaleCarrello(@RequestParam int utenteId) {
+        log.debug("calcolaTotaleCarrello: utenteId=" + utenteId);
+        ResponseObject<Double> response = new ResponseObject<>();
+        response.setRc(true);
+        try {
+            double totale = carrelloS.calcolaTotaleCarrello(utenteId);
+            response.setDati(totale);
+        } catch (Exception e) {
+            response.setMsg(e.getMessage());
+            response.setRc(false);
+        }
+        return response;
+    }
 
 
 
--- backendEcommerce/src/main/java/com/betacom/bec/services/implementations/CarrelloImpl.java
+++ backendEcommerce/src/main/java/com/betacom/bec/services/implementations/CarrelloImpl.java
@@ -1,8 +1,10 @@
 package com.betacom.bec.services.implementations;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.betacom.bec.models.Carrello;
 import com.betacom.bec.models.Prodotto;
@@ -12,6 +14,7 @@ import com.betacom.bec.repositories.ProdottoRepository;
 import com.betacom.bec.repositories.UtenteRepository;
 import com.betacom.bec.services.interfaces.CarrelloServices;
 
+@Service
 public class CarrelloImpl implements CarrelloServices{
 
 	@Autowired
@@ -24,22 +27,61 @@ public class CarrelloImpl implements CarrelloServices{
 	
 	@Autowired
 	Logger log;
+	
+	@Override
+	public Carrello aggiungiProdotto(int utenteId, int prodottoId, int quantita) {
+	    Utente utente = utenteRepository.findById(utenteId)
+	            .orElseThrow(() -> new RuntimeException("Utente non trovato"));
+	    Prodotto prodotto = prodottoRepository.findById(prodottoId)
+	            .orElseThrow(() -> new RuntimeException("Prodotto non trovato"));
+
+	    // Se il prodotto non è nel carrello, crea un nuovo elemento
+	    if (quantita <= 0) {
+	        throw new RuntimeException("La quantità deve essere maggiore di zero.");
+	    }
+
+	    Carrello nuovoCarrello = new Carrello();
+	    nuovoCarrello.setUtente(utente);
+	    nuovoCarrello.setProdotto(prodotto);
+	    nuovoCarrello.setQuantita(quantita);
+	    
+	    // Salva il nuovo carrello
+	    return carR.save(nuovoCarrello);
+	}
+
 
 	@Override
-    public Carrello aggiungiProdotto(int utenteId, int prodottoId, int quantita) {
-        Utente utente = utenteRepository.findById(utenteId)
-                .orElseThrow(() -> new RuntimeException("Utente non trovato"));
-        Prodotto prodotto = prodottoRepository.findById(prodottoId)
-                .orElseThrow(() -> new RuntimeException("Prodotto non trovato"));
+	public Carrello aggiornaPrezziCarrello(int utenteId) {
+	    List<Carrello> carrelloUtente = carR.findByUtenteId(utenteId);
+	    
+	    double totaleCarrello = 0;
+	    
+	    for (Carrello carrello : carrelloUtente) {
+	        double prezzoTotaleProdotto = carrello.getProdotto().getPrezzo() * carrello.getQuantita();
+	        carrello.setPrezzo(prezzoTotaleProdotto);
+	        totaleCarrello += prezzoTotaleProdotto; // Somma il prezzo totale
+	    }
 
-        Carrello carrello = new Carrello();
-        carrello.setUtente(utente);
-        carrello.setProdotto(prodotto);
-        carrello.setQuantita(quantita);
-        return carR.save(carrello);
-    }
+	    // Aggiorna e restituisci il carrello con i prezzi aggiornati
+	    carR.saveAll(carrelloUtente);
+	    return carrelloUtente.isEmpty() ? null : carrelloUtente.get(0);  // Restituisci il primo carrello (o gestisci come preferisci)
+	}
 
-    @Override
+	@Override
+	public double calcolaTotaleCarrello(int utenteId) {
+	    List<Carrello> carrelloUtente = carR.findByUtenteId(utenteId);
+	    
+	    double totaleCarrello = 0;
+	    for (Carrello carrello : carrelloUtente) {
+	        totaleCarrello += carrello.getPrezzo();
+	    }
+
+	    return totaleCarrello;
+	}
+	
+	
+
+	@Override
     public void rimuoviProdotto(int carrelloId) {
     	carR.deleteById(carrelloId);
     }
