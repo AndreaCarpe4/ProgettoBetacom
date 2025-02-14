8fd3de931266aa82e2e4875f1223f908fce79dc8
AndreaCarpe4 <andreacarpentieri1998@outlook.it>
2025-02-14 12:59:13 +0100

    Modifiche per metodo create recensione, che la crea solo se utente ha acquistato prodotto

--- backendEcommerce/src/main/java/com/betacom/bec/repositories/OrdineRepository.java
+++ backendEcommerce/src/main/java/com/betacom/bec/repositories/OrdineRepository.java
@@ -1,9 +1,17 @@
 package com.betacom.bec.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.betacom.bec.models.Ordine;
+import com.betacom.bec.models.Prodotto;
+import com.betacom.bec.models.Utente;
 
 public interface OrdineRepository extends JpaRepository<Ordine, Integer> {
+	
+	List<Ordine> findByCarrelloId(Integer idCarrello);
+
+	boolean existsByUtenteAndProdotto(Utente utente, Prodotto prodotto);
 
 }
