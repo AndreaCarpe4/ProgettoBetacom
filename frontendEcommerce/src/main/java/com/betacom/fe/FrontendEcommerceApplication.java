a1053dce7f144274e0f2679e7a26de4c7e0c7c17
AndreaCarpe4 <andreacarpentieri1998@outlook.it>
2025-02-17 12:26:27 +0100

    frontend

---
+++ frontendEcommerce/src/main/java/com/betacom/fe/FrontendEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendEcommerceApplication.class, args);
+	}
+
+}
